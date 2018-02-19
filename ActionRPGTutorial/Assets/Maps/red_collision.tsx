bf525db15b0d22c0989f56e418a6dcafc05a3fa0
ChrisCurreri <ccurreri95@gmail.com>
2018-02-19 14:52:58 -0500

    Unity RPG Tutorial #7- More Mapping Goodness

---
+++ ActionRPGTutorial/Assets/Maps/red_collision.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="red_collision" tilewidth="16" tileheight="16" spacing="1" tilecount="1" columns="1">
+ <image source="../Art/red-collision-box.png" width="16" height="16"/>
+ <tile id="0">
+  <objectgroup draworder="index">
+   <object id="1" x="0.25" y="0.125" width="15.625" height="15.625"/>
+  </objectgroup>
+ </tile>
+</tileset>
