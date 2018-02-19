f1256187c3eb55a9bbe1b0efa47bbb00095526e9
ChrisCurreri <ccurreri95@gmail.com>
2018-02-19 14:02:31 -0500

    My Project Files
    I put the project files into the repo.

---
+++ ActionRPGTutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    private Animator anim;
+    private bool playerMoving;
+    private Vector2 lastMove;
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        playerMoving = false;
+
+        if (moveX > 0.5f || moveX < -0.5f) // if player is pressing right or left
+        {
+            transform.Translate(new Vector3(moveX * moveSpeed * Time.deltaTime, 0f, 0f));
+            playerMoving = true;
+            lastMove = new Vector2(moveX, 0f);
+        }
+
+        if (moveY > 0.5f || moveY < -0.5f) // if player is pressing up or down
+        {
+            transform.Translate(new Vector3(0f, moveY * moveSpeed * Time.deltaTime, 0f));
+            playerMoving = true;
+            lastMove = new Vector2(0f, moveY);
+        }
+
+        anim.SetFloat("MoveX", moveX);
+        anim.SetFloat("MoveY", moveY);
+        anim.SetBool("PlayerMoving", playerMoving);
+        anim.SetFloat("LastMoveX", lastMove.x);
+        anim.SetFloat("LastMoveY", lastMove.y);
+    }
+}
